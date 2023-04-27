359bbda850e599f3713b41724e22019a950b9f6d
figueiredoAF <figueiredoaf1@gmail.com>
2023-04-27 19:08:28 -0300

    AT Server working

--- edge-impulse/ingestion-sdk-platform/espressif_esp32/ei_device_espressif_esp32.cpp
+++ edge-impulse/ingestion-sdk-platform/espressif_esp32/ei_device_espressif_esp32.cpp
@@ -56,12 +56,55 @@ void (*sample_cb_ptr)(void);
 /* Private function declarations ------------------------------------------- */
 void vTimerCallback(TimerHandle_t xTimer);
 
+#include "driver/usb_serial_jtag.h"
+#include "esp_vfs_usb_serial_jtag.h"
+#include "esp_vfs_dev.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+void initUart() {
+
+
+    /* Disable buffering on stdin */
+    setvbuf(stdin, NULL, _IONBF, 0);
+
+    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+    esp_vfs_dev_usb_serial_jtag_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+    /* Move the caret to the beginning of the next line on '\n' */
+    esp_vfs_dev_usb_serial_jtag_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+
+    /* Enable non-blocking mode on stdin and stdout */
+    fcntl(fileno(stdout), F_SETFL, 0);
+    fcntl(fileno(stdin), F_SETFL, 0);
+
+    usb_serial_jtag_driver_config_t usb_serial_jtag_config;
+    usb_serial_jtag_config.rx_buffer_size = 1024;
+    usb_serial_jtag_config.tx_buffer_size = 1024;
+
+    esp_err_t ret = ESP_OK;
+    /* Install USB-SERIAL-JTAG driver for interrupt-driven reads and writes */
+    ret = usb_serial_jtag_driver_install(&usb_serial_jtag_config);
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    /* Tell vfs to use usb-serial-jtag driver */
+    esp_vfs_usb_serial_jtag_use_driver();
+
+}
+
 /* Public functions -------------------------------------------------------- */
+
+
+
 
 EiDeviceESP32::EiDeviceESP32(EiDeviceMemory* mem)
 {
 
     EiDeviceInfo::memory = mem;
+
+    initUart();
 
     init_device_id();
 
